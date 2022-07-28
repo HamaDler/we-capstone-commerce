b863319f047e134e2615232bcfbb83551a128510
hamadler <hamadler58@gmail.com>
2022-07-28 15:48:12 +0300

    config: add tailwind

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
+// global state object (store)
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
 });
