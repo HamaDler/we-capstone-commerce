7095832abb956e3b1e6b182e9c064ef562bc57cb
hamadler <hamadler58@gmail.com>
2022-07-28 16:14:42 +0300

    config: add route definitions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Products from './pages/Products';
+import Product from './pages/Product';
 
 function App() {
   return (
     <div>
-      {' '}
-      <h1 className="text-3xl font-bold underline bg-red-400">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<Product />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const container = document.getElementById('root');
@@ -9,6 +11,8 @@ const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
