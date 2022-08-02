22d20b4bc41d44d188c5fcac2cbd28abd9f8c248
Hama Dler <hamadler48@gmail.com>
2022-08-02 17:35:01 +0300

    refactor: add loading spinner component

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,5 +1,34 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Layout from '../layouts/MainLayout';
+import LoadingSpinner from '../components/LoadingSpinner';
 export default function Product() {
-  return <div>Product</div>;
+  let { id } = useParams();
+
+  const [product, setProduct] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .then((response) => {
+        console.log('list of products:', response.data);
+        setProduct(response.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Layout>
+        <LoadingSpinner />
+      </Layout>
+    );
+  }
+
+  return <Layout> {JSON.stringify(product)}</Layout>;
 }
