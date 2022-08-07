f70a36ebbad3f377084c52f1260ae93b4679d746
Hama Dler <hamadler48@gmail.com>
2022-08-07 16:49:27 +0300

    feat: add react modal

---
+++ src/components/ReactModal.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import ReactCarousel from './ReactCarousel';
+export default function ReactModal() {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        // style={customStyles}
+        contentLabel="Example Modal"
+        // className={'bg-slate-900'}
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <ReactCarousel />
+      </Modal>
+    </div>
+  );
+}
--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import ReactModal from '../components/ReactModal';
 import Layout from '../layouts/MainLayout';
 export default function Categories() {
-  return <Layout>Categories</Layout>;
+  return (
+    <Layout>
+      <ReactModal />
+    </Layout>
+  );
 }
