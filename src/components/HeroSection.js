253c37240b17c06902ffc2b3e5f34d1463021af4
hamadler <hamadler58@gmail.com>
2022-08-08 10:23:37 +0300

    hotfix: add i18n translation namespace

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactCarousel from './ReactCarousel';
 import { useTranslation } from 'react-i18next';
 export default function HeroSection() {
-  const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation('common');
 
   return (
     <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 mt-8 md:mt-10 ">
       <div className=" flex flex-col items-center justify-center">
         <h2 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          {/* {t('home.title')} */}
-          {/* <span className="text-indigo-600"> {t('Shopping ')}</span> */}
+          {t('welcome.title')}
+          <span className="text-indigo-600">{t('welcome.shop')}</span>
         </h2>
-        <p className="text-2xl text-gray-500 mt-3">{t('Welcome.title')}</p>
+        <p className="text-2xl text-gray-500 mt-3">{t('welcome.title')}</p>
         <div className="mt-10">
           <button className="shadow-md hover:shadow-lg bg-indigo-600 text-white px-6 py-4 rounded-lg text-xl font-semibold mr-4">
             View Products
--- src/i18n.js
+++ src/i18n.js
@@ -3,21 +3,23 @@ import { initReactI18next } from 'react-i18next';
 import common_ku from './translations/ku/common.json';
 import common_en from './translations/en/common.json';
 import common_ar from './translations/ar/common.json';
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+
+const resources = {
+  en: {
+    common: common_en, // 'common' is our custom namespace
+  },
+  ku: {
+    common: common_ku,
+  },
+  ar: {
+    common: common_ar,
+  },
+};
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    resources: {
-      en: {
-        common: common_en, // 'common' is our custom namespace
-      },
-      ku: {
-        common: common_ku,
-      },
-    },
+    resources,
     lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
